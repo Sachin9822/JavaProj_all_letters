3860e3256c994d55b4c927e3ced422484fcdf8c7
Sachin9822 <sachinhacker001@gmail.com>
2022-07-04 22:54:02 +0530

    added gui

---
+++ app/src/main/java/com/example/all_letters/leave_page.java
@@ -0,0 +1,13 @@
+package com.example.all_letters;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class leave_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leave_page);
+    }
+}
--- app/src/main/java/com/example/all_letters/templates.java
+++ app/src/main/java/com/example/all_letters/templates.java
@@ -1,14 +1,49 @@
 package com.example.all_letters;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class templates extends AppCompatActivity {
-
+public class templates extends AppCompatActivity implements View.OnClickListener {
+    public  CardView c4,c5,c6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_templates);
+        c4=(CardView) findViewById(R.id.appology_card);
+        c5=(CardView) findViewById(R.id.leave_card);
+        c6=(CardView) findViewById(R.id.request_card);
+
+        c4.setOnClickListener(this);
+        c5.setOnClickListener(this);
+        c6.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()){
+
+            case R.id.appology_card:
+                i=new Intent(this,apology_page.class);
+                startActivity(i);
+                break;
+
+            case R.id.leave_card:
+                i=new Intent(this,leave_page.class);
+                startActivity(i);
+                break;
+
+            case R.id.request_card:
+                i=new Intent(this,request_page.class);
+                startActivity(i);
+                break;
+        }
+
     }
 }
